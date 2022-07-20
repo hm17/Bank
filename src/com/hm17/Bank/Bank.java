e04679c2a9b0623a5fbcaf270de07c2cfb040082
hazel <hazel.madolid@gmail.com>
2022-07-20 11:22:20 -0500

    first commit

---
+++ src/com/hm17/Bank/Bank.java
@@ -0,0 +1,12 @@
+package com.hm17.Bank;
+
+import com.hm17.Bank.Account;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		
+		Account hazel = new Account("Hazel M", "10000");
+		hazel.showMenu();
+	}
+}
